2b5824046dd1f84a94352e0bdfa0deabf5579fcb
Filipe Zaidan <41112779+filipezaidan@users.noreply.github.com>
2021-03-11 21:05:12 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image} from 'react-native';
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
